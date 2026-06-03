c6d1a97aa871f7c40d1760b9bd2791db043c3cb2
reethika-ai <reethikagunna@gmail.com>
2026-06-03 11:10:14 +0530

    Connect frontend to deployed backend

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -3,19 +3,23 @@ export async function sendMessage(
   message: string,
   code?: string
 ) {
-  const res = await fetch("http://127.0.0.1:8000/chat", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      student_id: studentId,
-      message,
-      code,
-    }),
-  });
+  const res = await fetch(
+    "https://ai-socratic-dsa-assistant-8.onrender.com/chat",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        student_id: studentId,
+        message,
+        code,
+      }),
+    }
+  );
 
   if (!res.ok) {
+    console.log(await res.text());
     throw new Error("Failed to connect to backend");
   }
 
